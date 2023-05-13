2d35f9d7f29c21e96eebc6cf053e3d2f892d161b
spark3i <djubreel09@gmail.com>
2023-05-13 10:29:44 -0700

    code that passes betty checks

---
+++ simple_shell.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  *main- checks if a number is even or odd
+  *Return: 0 when succesful
+  */
+int main(void)
+{
+	int i;
+
+	if (i % 2 == 0)
+	{
+		printf("%d is even\n", i);
+	}
+	else
+	{
+		printf("%d is odd\n", i);
+	}
+	return (0);
+}
